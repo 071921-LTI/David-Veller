3fdf02e7933267490f6f61bd60757d3f06e21f3d
dvellerlti <david.veller@lntinfotech.com>
2021-07-28 12:43:24 -0400

    changed .gitignore

--- project0v2/src/main/java/com/lti/models/User.java
+++ project0v2/src/main/java/com/lti/models/User.java
@@ -1,5 +1,129 @@
 package com.lti.models;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable{
+	
+	private int id;
+	private String user;
+	private String pass;
+	private String role; //customer, employee, or manager
+	
+	
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", user=" + user + ", pass=" + pass + ", role=" + role + "]";
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((pass == null) ? 0 : pass.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		if (pass == null) {
+			if (other.pass != null)
+				return false;
+		} else if (!pass.equals(other.pass))
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getUser() {
+		return user;
+	}
+
+
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+
+
+	public String getPass() {
+		return pass;
+	}
+
+
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+
+	public User() {
+		super();
+	}
+
+
+
+	public User(int id, String user, String pass, String role) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.pass = pass;
+		this.role = role;
+	}
+	
+	
 
 }
